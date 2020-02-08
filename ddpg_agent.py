9cad5466b243b861f436b840fbdeb22a61c107ce
Jeremy <crasdmek@yahoo.com>
2020-02-08 16:48:35 -0500

    Environment solved in 1242 episodes!

--- ddpg_agent.py
+++ ddpg_agent.py
@@ -10,8 +10,8 @@ import torch.nn.functional as F
 import torch.optim as optim
 
 BUFFER_SIZE = int(1e6)  # replay buffer size
-BATCH_SIZE = 1024        # minibatch size
-GAMMA = 0.8            # discount factor
+BATCH_SIZE = 1024       # minibatch size
+GAMMA = 0.99            # discount factor
 TAU = 1e-3              # for soft update of target parameters
 LR_ACTOR = 2e-4         # learning rate of the actor 
 LR_CRITIC = 2e-4        # learning rate of the critic
